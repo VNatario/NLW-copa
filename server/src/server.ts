ea2aeaef8b79497f467ab4e4cf2b2640c683b9c5
Vitor Natario <natariovitor@gmail.com>
2022-11-01 10:40:44 -0300

    create route /pools/count

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,15 @@ import Fastify from "fastify";
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
+    //habilita logs
+  });
+
+  //Rota de contagem (GET)
+  //http:localhost:3333/pools/count
+  fastify.get("/pools/count", () => {
+    return {
+      count: 1123412343,
+    };
   });
 
   await fastify.listen({ port: 3333 });
