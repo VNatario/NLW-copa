1191ea23a346ec53026809a1fa8db564c60513a5
Vitor Natario <natariovitor@gmail.com>
2022-11-01 11:38:49 -0300

    count in pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,9 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
@@ -8,10 +12,10 @@ async function bootstrap() {
 
   //Rota de contagem (GET)
   //http:localhost:3333/pools/count
-  fastify.get("/pools/count", () => {
-    return {
-      count: 113,
-    };
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
   });
 
   await fastify.listen({ port: 3333 });
